760770815884408f54858b0e32cabb21e5f84335
ArwaAlzahrani <rookaturki@hotmail.com>
2022-05-31 07:58:00 +0300

    Initial Commit

---
+++ TakeTech/TakeTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeTechApp.swift
+//  TakeTech
+//
+//  Created by Arwa Alzahrani on 01/11/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct TakeTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
