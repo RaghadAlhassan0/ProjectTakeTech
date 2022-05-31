087bd9fe6e36391337158ea59a5c4cf86bbb8a8c
Arwatech <rookaturki@hotmail.com>
2022-05-31 07:27:57 +0300

    first

---
+++ TestGithub/TestGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGithubApp.swift
+//  TestGithub
+//
+//  Created by Arwa Alzahrani on 01/11/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct TestGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
